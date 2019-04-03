a9a9a97d7e4d294dbd911999f939ea83b2d29d29
AlexSim <jlmaj_sim@naver.com>
2019-04-03 17:17:50 +0900

    ch5

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE) # 사용자를 지우면 모든 Post를 지우겠다는 것
+    body = models.TextField() 
+
+    def __str__(self):
+        return self.title
